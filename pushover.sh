49decb26f85bea8e4532e2a666b921a2b4a091a5
Rambo <RamboRigs90@gmail.com>
2021-10-01 11:06:45 -0400

    Updated pushover

--- pushover.sh
+++ pushover.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
+# Pushover script by RamboRigs
+# Usage: ./pushover message title* sound* url* apptoken*
+
 MESSAGE=$1
 TITLE=$2
 SOUND=$3
 APP_URL=$4
 APP_TOKEN=$5
+
+#FILL IN THE DEFAULT VALUES BELOW:
 USER_TOKEN="YOUR USER KEY HERE"
-DEFAULT_APP="DFAULT APP KEY HERE"
-DEFAULT_SOUND="R2D2"
-DEFAULT_URL=" "
+DEFAULT_APP="DEFAULT APP KEY HERE"
+DEFAULT_SOUND="DEFAULT SOUND HERE" #can be left blank (" ")
+DEFAULT_URL="DEFAULT URL HERE" #can be left blank (" ")
+DEFAULT_TITLE="DEFAULT TITLE HERE" #can be left blank (" ")
+
 
 if [ $# -eq 0 ]; then
     echo "Usage: ./pushover message title* sound* url* apptoken*"
@@ -15,13 +22,13 @@ if [ $# -eq 0 ]; then
     exit
 fi
 if [ $# -lt 2 ]; then
-	TITLE="DEFAULT TITLE"
-    SOUND=$DEFAULT_SOUND
+	TITLE=$DEFAULT_TITLE
+	SOUND=$DEFAULT_SOUND
 	APP_URL=$DEFAULT_URL
 	APP_TOKEN=$DEFAULT_APP
 fi
 if [ $# -lt 3 ]; then
-    SOUND=$DEFAULT_SOUND
+	SOUND=$DEFAULT_SOUND
 	APP_URL=$DEFAULT_URL
 	APP_TOKEN=$DEFAULT_APP
 fi
