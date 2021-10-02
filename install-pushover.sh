c0c83795ed900fc06273cba150c2f2c21c84d31d
Rambo <RamboRigs90@gmail.com>
2021-10-02 14:48:49 -0400

    delimeter fix

--- install-pushover.sh
+++ install-pushover.sh
@@ -10,8 +10,8 @@ BASEDIR=$(dirname "$0")
 sed -i "s/USER_TOKEN=/USER_TOKEN=\"$2\"/g" $BASEDIR/pushover.sh
 sed -i "s/DEFAULT_APP=/DEFAULT_APP=\"$3\"/g" $BASEDIR/pushover.sh
 sed -i "s/DEFAULT_SOUND=/DEFAULT_SOUND=\"$4\"/g" $BASEDIR/pushover.sh
-sed -i "s+DEFAULT_URL=+DEFAULT_URL=\"$5\"+g" $BASEDIR/pushover.sh
+sed -i "s+DEFAULT_URL=+DEFAULT_URL=\"$5\"+g" $BASEDIR/pushover.sh #changed delimeter to + to avoid issues with url (//)
 sed -i "s/DEFAULT_TITLE=/DEFAULT_TITLE=\"$1\"/g" $BASEDIR/pushover.sh
 
-#mv $BASEDIR/pushover.sh /usr/bin/pushover
-#chmod +x /usr/bin/pushover
+mv $BASEDIR/pushover.sh /usr/bin/pushover
+chmod +x /usr/bin/pushover
